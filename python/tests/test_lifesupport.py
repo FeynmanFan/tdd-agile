7d7baf5a31cfaeb99ecb0099f4e13cda8e2b8b47
FeynmanFan <cbehrens@gmail.com>
2025-04-08 17:02:51 -0500

    Removed tests to break coverage

--- python/tests/test_lifesupport.py
+++ python/tests/test_lifesupport.py
@@ -21,19 +21,3 @@ def test_oxygen_level_drops_below_threshold_and_is_replenished():
     assert "Oxygen replenished to 80%" == result
 
     # raise NotImplementedError("Don't know correct yet")
-
-def test_oxygen_level_at_20():
-    lss = LifeSupportSystem(isActive=True)
-    lss.set_oxygen_level(20)
-
-    result = lss.check_oxygen_level()
-
-    assert "Warning: Oxygen level at 20%" == result
-
-def test_oxygen_level_at_50():
-    lss = LifeSupportSystem(isActive=True)
-    lss.set_oxygen_level(50)
-
-    result = lss.check_oxygen_level()
-
-    assert "Oxygen level stable at 50%" == result
