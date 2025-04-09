9c878ddff98e633a9d729e06fe64a666e4097267
FeynmanFan <cbehrens@gmail.com>
2025-04-09 11:53:36 -0500

    Added c-sharp code and unit test project

---
+++ c-sharp/tdd-agile.lifesupport.tests/WaterLevelMonitorTests.cs
@@ -0,0 +1,61 @@
+﻿namespace tdd_agile.lifesupport.tests
+{
+    using System;
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        public class WaterLevelMonitorTests
+        {
+            [Test]
+            public void Constructor_ValidInput_SetsInitialLevel()
+            {
+                var monitor = new WaterLevelMonitor(100);
+                Assert.That(monitor.GetWaterLevel(), Is.EqualTo(100));
+            }
+
+            [Test]
+            public void AddWater_PositiveAmount_IncreasesLevel()
+            {
+                var monitor = new WaterLevelMonitor(50);
+                monitor.AddWater(20);
+                Assert.That(monitor.GetWaterLevel(), Is.EqualTo(70));
+            }
+
+            [Test]
+            public void UseWater_ValidAmount_DecreasesLevel()
+            {
+                var monitor = new WaterLevelMonitor(50);
+                monitor.UseWater(20);
+                Assert.That(monitor.GetWaterLevel(), Is.EqualTo(30));
+            }
+
+            [Test]
+            public void Constructor_NegativeInitial_ThrowsArgumentException()
+            {
+                Assert.Throws<ArgumentException>(() => new WaterLevelMonitor(-10));
+            }
+
+            [Test]
+            public void AddWater_NegativeAmount_ThrowsArgumentException()
+            {
+                var monitor = new WaterLevelMonitor(50);
+                Assert.Throws<ArgumentException>(() => monitor.AddWater(-5));
+            }
+
+            [Test]
+            public void UseWater_NegativeAmount_ThrowsArgumentException()
+            {
+                var monitor = new WaterLevelMonitor(50);
+                Assert.Throws<ArgumentException>(() => monitor.UseWater(-10));
+            }
+
+            [Test]
+            public void UseWater_ExceedsCurrentAmount_ThrowsInvalidOperationException()
+            {
+                var monitor = new WaterLevelMonitor(50);
+                Assert.Throws<InvalidOperationException>(() => monitor.UseWater(60));
+            }
+        }
+    }
+}
---
+++ c-sharp/tdd-agile.lifesupport/WaterLevelMonitor.cs
@@ -0,0 +1,34 @@
+﻿namespace tdd_agile.lifesupport
+{
+    using System;
+
+    public class WaterLevelMonitor
+    {
+        private double _waterLevel;
+
+        public WaterLevelMonitor(double initialWaterLevel)
+        {
+            if (initialWaterLevel < 0)
+                throw new ArgumentException("Water level cannot be negative.");
+            _waterLevel = initialWaterLevel;
+        }
+
+        public double GetWaterLevel() => _waterLevel;
+
+        public void AddWater(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Cannot add negative water.");
+            _waterLevel += amount;
+        }
+
+        public void UseWater(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Cannot use negative water.");
+            if (amount > _waterLevel)
+                throw new InvalidOperationException("Insufficient water.");
+            _waterLevel -= amount;
+        }
+    }
+}
